6317b859303a08e143fba62d371cb6a368a296c6
Isaias Puente <i.puente33@gmail.com>
2014-11-10 17:17:45 -0600

    JUnit Testing
    Added three junit skeleton classes to test our model, view, and
    controller separately

---
+++ GoL Sample/gol-mvc/gol/test/TestModel.java
@@ -0,0 +1,14 @@
+package gol.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestModel {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
