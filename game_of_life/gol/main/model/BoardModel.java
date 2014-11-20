8461fe7decd37d8939070b20ba5257528e2a8809
Kyle Haley <kylephaley@gmail.com>
2014-11-20 12:49:53 -0600

    Factored out board configuration from BoardModel

--- game_of_life/gol/main/model/BoardModel.java
+++ game_of_life/gol/main/model/BoardModel.java
@@ -6,51 +6,44 @@ import java.util.Random;
 
 public class BoardModel {
 	
-	private static final int BLOCK_SIZE = 10;
-	
-	private int movesPerSecond = 3;
-	
-	private int gameBoard_Width = 0;
-	
-	private int gameBoard_Height = 0;
-	
 	private ArrayList<Point> points;
+	
+	private BoardConfigModel boardConfigModel;
 	
 	public BoardModel(int height, int width){
 		this.points = new ArrayList<Point>(0);
-		this.gameBoard_Height = height;
-		this.gameBoard_Width = width;
+		this.boardConfigModel = new BoardConfigModel(width, height);
 	}
 	
 	public void setMovesPerSecond(int movesPerSec){
-		this.movesPerSecond = movesPerSec;
+		this.boardConfigModel.movesPerSecond = movesPerSec;
 	}
 	
 	public int getMovesPerSecond(){
-		return this.movesPerSecond;
+		return this.boardConfigModel.movesPerSecond;
 	}
 	
 	public void setGameBoardHeight(int height){
-		this.gameBoard_Height = height;
+		this.boardConfigModel.gameBoard_Height = height;
 	}
 	
 	public void setGameBoardWidth(int width){
-		this.gameBoard_Width = width;
+		this.boardConfigModel.gameBoard_Width = width;
 	}
 	
 	public int getGameBoardHeight(){
-		return this.gameBoard_Height;
+		return this.boardConfigModel.gameBoard_Height;
 	}
 	
 	public int getGameBoardWidth(){
-		return this.gameBoard_Width;
+		return this.boardConfigModel.gameBoard_Width;
 	}
 	
 	public void updateArraySize() { 
         ArrayList<Point> removeList = new ArrayList<Point>(0);
         for (Point current : points) {
-            if ((current.x > this.gameBoard_Width-1) 
-            		|| (current.y > this.gameBoard_Height-1)) {
+            if ((current.x > this.getGameBoardWidth() - 1) 
+            		|| (current.y > this.getGameBoardHeight() - 1)) {
                 removeList.add(current);
             }
         }
@@ -78,8 +71,8 @@ public class BoardModel {
 
     public void randomlyFillBoard(int percent) { 
     	Random test = new java.util.Random();
-        for (int i=0; i < this.gameBoard_Width; i++) {
-            for (int j=0; j < this.gameBoard_Height; j++) {
+        for (int i=0; i < this.getGameBoardWidth(); i++) {
+            for (int j=0; j < this.getGameBoardHeight(); j++) {
                 if (test.nextInt(101) < percent) {
                 	// this method is used when 'points' has been cleared,
                 	// so assume adding to empty list to save performance.
@@ -91,15 +84,15 @@ public class BoardModel {
     
     public void updateModel(){
     	boolean[][] gameBoard = 
-    			new boolean[ this.gameBoard_Width + 2 ]
-    					   [ this.gameBoard_Height + 2 ];
+    			new boolean[ this.getGameBoardWidth() + 2 ]
+    					   [ this.getGameBoardHeight() + 2 ];
     	for (Point current : points) {
     		gameBoard[(current.x+1)][(current.y+1)] = true;
         }
         ArrayList<Point> survivingCells = new ArrayList<Point>(0);
         // Iterate through the array, follow game of life rules
-        for (int i=1; i<gameBoard.length-1; i++) {
-            for (int j=1; j<gameBoard[0].length-1; j++) {
+        for (int i=1; i < gameBoard.length - 1; i++) {
+            for (int j=1; j < gameBoard[0].length - 1; j++) {
                 int surrounding = 0;
                 if (gameBoard[i-1][j-1]) { surrounding++; }
                 if (gameBoard[i-1][j])   { surrounding++; }
@@ -127,6 +120,6 @@ public class BoardModel {
     }
     
     public static int getBlockSize() { // model function
-		return BLOCK_SIZE;
+		return BoardConfigModel.BLOCK_SIZE;
 	}
 }
